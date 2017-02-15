df9ce628f96537ce925afb737c6e79e07f90f6ea
Ryan <ryanisinallofus@gmail.com>
2017-02-14 16:39:57 -0800

    First stab at a denim file

---
+++ denim.js
@@ -0,0 +1,30 @@
+module.exports = {
+  destination: {
+    default: function (data) {
+      return data.name;
+    }
+  },
+
+  prompts: {
+      Name: {
+        message: "Name your component (Capitalized): ",
+        validate: function (val) {
+          if (!!val.trim()) {
+            return "A Name must be entered!"
+          } elseif (val[0] !== x[0].toUppercase()) {
+            return "Name must be uppercase!"
+          }
+        }
+      },
+      name: {
+        message: "Name your component (lowercase): ",
+        validate: function (val) {
+          if (!!val.trim()) {
+            return "A name must be entered!"
+          } elseif (val[0] !== x[0].toLowercase()) {
+            return "name must be lowercase!"
+          }
+        }
+      }
+  }
+};
